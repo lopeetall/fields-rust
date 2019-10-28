18958da374dfa03bb527827830e7c3e9da7eb770
Joshua Fitzgerald <joshuabfitzgerald@gmail.com>
2019-10-28 09:13:32 -0400

    Transition from limb-by-limb mod to digit-by-digit mod

--- src/bigint.rs
+++ src/bigint.rs
@@ -3,16 +3,15 @@
 // MAKE IT IMPRESSIVE
 
 // Implement:
-// Shift Left **
-// AddAssign **
-// SubAssign **
-// MulAssign **
-// Make Bigint indexable and remove the .lists when necessary **
-// give Bigints a len() function and remove .list.len() when necessary **
 // ---MAke Bigint iterable and remove the .into_iter when necessary (maybe not?)---
 
 // mod
-// mod assign
+// mod assign?
+
+// square?
+
+// mod exp!!! (necessary--actually the big goal)
+
 // int div
 // int div assign
 
@@ -25,7 +24,9 @@ use std::fmt;
 use std::ops;
 use std::cmp;
 
-#[derive(Debug)]
+const max: u64 = std::u64::MAX;
+
+#[derive(Debug, Clone)]
 pub struct Bigint {
     pub list: Vec<u64>,
 }
@@ -40,9 +41,9 @@ impl Bigint {
     pub fn add_u64_with_carry (first: &u64, other: &u64) -> Vec<u64> {
         let c: u64;
         let s: u64;
-        if first > &(0xFFFFFFFFFFFFFFFF - other) {
+        if first > &(max - other) {
             c = 1;
-            s = (first - (0xFFFFFFFFFFFFFFFF - other)) - 0x1;
+            s = (first - (max - other)) - 0x1;
         } else {
             c = 0;
             s = first + other;
@@ -60,7 +61,7 @@ impl Bigint {
         let d: u64;
         if first < other {
             b = 1;
-            d = ((0xFFFFFFFFFFFFFFFF - other) + 0x1) + first;
+            d = ((max - other) + 0x1) + first;
         } else {
             b = 0;
             d = first - other;
@@ -74,11 +75,13 @@ impl Bigint {
         let leftq = vec![left / sqrt, left % sqrt];
         let rightq = vec![right / sqrt, right % sqrt];
         let tmp_middle = [(leftq[0]*rightq[1]) / sqrt + (leftq[1]*rightq[0]) / sqrt, (leftq[0]*rightq[1]) % sqrt + (leftq[1]*rightq[0]) % sqrt];
-        let (big, middle, little) = (leftq[0]*rightq[0] + tmp_middle[0], tmp_middle[1]*sqrt, leftq[1]*rightq[1]);
+        let lower_middle = [tmp_middle[1] / sqrt, tmp_middle[1] % sqrt];
+        let (big, middle, little) = (leftq[0]*rightq[0] + tmp_middle[0] + lower_middle[0], lower_middle[1]*sqrt, leftq[1]*rightq[1]);
         let littles = Bigint::add_u64_with_carry(&middle, &little);
         vec![big + littles[0], littles[1]]
     }  
 
+    /*
     pub fn mul_by_u64 (&self, other: u64) -> Bigint {
         let n = self.len();
         let mut r = Bigint::new(vec![]);
@@ -86,7 +89,7 @@ impl Bigint {
             r += Bigint::new(Bigint::mul_u64_with_carry(self[i], other)) << n-1-i;
         }
         r
-    }
+    }*/
 
     pub fn pad (list: &Vec<u64>, n: usize) -> Vec<u64> {
         let mut z = vec![0; n-list.len()];
@@ -94,9 +97,121 @@ impl Bigint {
         z
     }
 
+    pub fn one () -> Bigint {
+        Bigint::new(vec![1])
+    }
+
+    pub fn zero () -> Bigint {
+        Bigint::new(vec![0])
+    }
+
+    pub fn base () -> Bigint {
+        Bigint::one() << 1
+    }
+
+    pub fn base_power (p: usize) -> Bigint {
+        Bigint::base() << p
+    }
+
+    pub fn same_size_mod(num: Bigint, divisor: Bigint) -> Bigint {
+        if num.len() != divisor.len() {
+            panic!("Number and divisor not the same length.");
+        }
+        if num < divisor {
+            num
+        } else {
+            let mut n = num.clone();
+            let d = divisor.clone();
+            let mut q = num[0] / (divisor[0] + 1);
+            let mut oldq = q.clone();
+            while d.clone()*q < n {
+                n -= d.clone()*q;
+                oldq = q;
+                q += n[0] / (d[0] + 1);
+            }
+            num - Bigint::new(vec![oldq]) * divisor
+        }
+
+    }
+
+
+
+    pub fn same_size_quot(num: Bigint, divisor: Bigint) -> Bigint {
+        if num.len() != divisor.len() {
+            panic!("Number and divisor not the same length for step_mod.");
+        }
+        if num < divisor {
+            num
+        } else {
+            let mut n = num.clone();
+            let d = divisor.clone();
+            let mut q = num[0] / (divisor[0] + 1);
+            let mut oldq = q.clone();
+            while d.clone()*q < n {
+                n -= d.clone()*q;
+                oldq = q;
+                q += n[0] / (d[0] + 1);
+            }
+            Bigint::new(vec![oldq])
+        }
+
+    }
+
+    pub fn first_digit_with_index (u: u64) -> (u64, u8) {
+        let mut s = 15;
+        while u >> 4*s == 0 {
+            s -= 1; 
+        }
+        (u >> 4*s, 15-s)
+    }
+
+    pub fn hex_digit_of_u64_at_index(u: u64, index: usize) -> u64 {
+        u >> 4*(15 - index)
+    }
+
+    pub fn shift_left (self, n: usize) -> Bigint {
+        let result = vec![hex_digit_of_u64_at_index(self[0], 0)];
+        for i in self {
+            
+        }
+        }
+    }
+
+    pub fn mul_by_u64_mod (self, coeff: u64, divisor: Bigint) -> Bigint {
+        let hlr = Bigint::same_size_mod(Bigint::new(vec![std::u64::MAX; divisor.len()]), divisor.clone()) + Bigint::one();
+        let mut r = self.clone()*coeff;
+        while r.len() > self.clone().len() {
+            let rtail = r.list.drain(1..).collect();
+            r = hlr.clone()*Bigint::one()*r + Bigint::same_size_mod(Bigint::new(rtail), divisor.clone());
+        }
+        Bigint::same_size_mod(r, divisor)
+    }
+
+    pub fn add_same_size_mod (self, other: Bigint, divisor: Bigint) -> Bigint {
+        let hlr = Bigint::same_size_mod(Bigint::new(vec![std::u64::MAX; divisor.len()]), divisor.clone()) + Bigint::one();
+        let mut r = self.clone()+other;
+        while r.len() > self.clone().len() {
+            let rtail = r.list.drain(1..).collect();
+            r = hlr.clone()*Bigint::one()*r + Bigint::same_size_mod(Bigint::new(rtail), divisor.clone());
+        }
+        Bigint::same_size_mod(r, divisor)
+    }
+/*
+    pub fn big_mod (self, divisor: Self) -> Bigint {
+        let mut result = self.clone();
+        let d = divisor.clone();
+        let mut s = result.len() - d.len();
+        let mut q = Bigint::one();
+        while result > divisor.clone() {
+            let currd = divisor.clone() << (s - 1);
+            while result > currd
+        }
+        result        
+    }
+*/
     pub fn depad (list: &Vec<u64>) -> Vec<u64> {
         // remove leading zeros before creating a new bigint
-        // leaves one zero element if the big int is actually 0
+        // leaves one zero element if the big int is identically 0
         // I would love to see a more functional way of doing this
         let mut r = vec![];
         let mut sigs_have_begun = false;
@@ -120,6 +235,10 @@ impl fmt::Display for Bigint {
     }
 }
 
+
+
+// FIX ADD FUNCTION WITH MULTIPLE CARRIES
+/*
 impl ops::Add for Bigint {
     type Output = Self;
     fn add (self, other: Self) -> Bigint {
@@ -130,7 +249,43 @@ impl ops::Add for Bigint {
         for i in (1..(sum_length)).rev() {
             let ac = Bigint::add_u64_with_carry(&l[i], &r[i]);
             sum[i] += ac[1];
-            sum[i-1] += ac[0]; 
+            sum[i-1] += ac[0];
+        }
+        Bigint::new(sum)
+    }
+}
+*/
+impl ops::Add for Bigint {
+    type Output = Self;
+    fn add (self, other: Self) -> Bigint {
+        let sum_length = cmp::max(self.len(), other.len()) + 1;
+        //let max = std::u64::MAX;
+        let mut sum: Vec<u64> = vec![0; sum_length];
+        let l = Bigint::pad(&self.list, sum_length);
+        let r = Bigint::pad(&other.list, sum_length);
+        for i in (1..(sum_length)).rev() {
+            let ac = Bigint::add_u64_with_carry(&l[i], &r[i]);
+            let ac2 = Bigint::add_u64_with_carry(&sum[i], &ac[1]);
+            sum[i] += ac2[1];
+            sum[i-1] += ac[0]+ac2[0]; 
+        }
+        Bigint::new(sum)
+    }
+}
+
+impl ops::Add<u64> for Bigint {
+    type Output = Self;
+    fn add (self, other_u64: u64) -> Bigint {
+        let other = Bigint::new(vec![other_u64]);
+        let sum_length = cmp::max(self.len(), other.len()) + 1;
+        let mut sum: Vec<u64> = vec![0; sum_length];
+        let l = Bigint::pad(&self.list, sum_length);
+        let r = Bigint::pad(&other.list, sum_length);
+        for i in (1..(sum_length)).rev() {
+            let ac = Bigint::add_u64_with_carry(&l[i], &r[i]);
+            let ac2 = Bigint::add_u64_with_carry(&sum[i], &ac[1]);
+            sum[i] += ac2[1];
+            sum[i-1] += ac[0]+ac2[0]; 
         }
         Bigint::new(sum)
     }
@@ -138,10 +293,10 @@ impl ops::Add for Bigint {
 
 impl ops::AddAssign for Bigint {
     fn add_assign (&mut self, other: Self) {
-        let placeholder = &self.list;
-        *self = Bigint::new(placeholder.to_vec()) + other;
+        *self = self.clone() + other;
     }
 }
+
 
 impl ops::Shl<usize> for Bigint {
     type Output = Self;
@@ -152,14 +307,27 @@ impl ops::Shl<usize> for Bigint {
     }
 }
 
+
 impl ops::Mul for Bigint {
     type Output = Self;
     fn mul (self, other: Self) -> Bigint {
         let n = other.len();
         let mut r = Bigint::new(vec![]);
         for i in (0..n).rev() {
-            r += self.mul_by_u64(other[i]) << n-1-i;
+            r += self.clone()*other[i] << n-1-i;
         }    
+        r
+    }
+}
+
+impl ops::Mul<u64> for Bigint {
+    type Output = Self;
+    fn mul (self, other: u64) -> Bigint {
+        let n = self.len();
+        let mut r = Bigint::new(vec![]);
+        for i in (0..n).rev() {
+            r += Bigint::new(Bigint::mul_u64_with_carry(self[i], other)) << n-1-i;
+        }
         r
     }
 }
@@ -180,8 +348,13 @@ impl ops::Sub for Bigint {
         let mut d: Vec<u64> = vec![0; n];
         for i in (0..n).rev() {
             if l[i] < r[i] {
-                l[i-1] -= 1; //borrow
-                d[i] = 0xFFFFFFFFFFFFFFFF - r[i] + 0x1 + l[i];
+                let mut j = i;
+                while l[j-1] == 0 {
+                    l[j-1] = max;
+                    j -= 1;
+                }
+                l[j-1] -= 1;
+                d[i] = (max - r[i]) + 0x1 + l[i];
             } else {
                 d[i] = l[i] - r[i];
             }
@@ -192,8 +365,7 @@ impl ops::Sub for Bigint {
 
 impl ops::SubAssign for Bigint {
     fn sub_assign (&mut self, other: Self) {
-        let placeholder = &self.list;
-        *self = Bigint::new(placeholder.to_vec()) - other;
+        *self = self.clone() - other
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 extern crate rand;
 
 use bigint::Bigint;
-//use rand::Rng;
+use rand::Rng;
 
 fn main () {
-       // let mut rng = rand::thread_rng();
-//let max = std::u64::MAX;
-let n1 = Bigint::new(vec![1, 0, 0]);
-let n2 = Bigint::new(vec![1, 0]);
+let mut rng = rand::thread_rng();
+let max = std::u64::MAX;
 
-println!("{}", n1);
-println!("{}", n2);
-println!("{}", n1*n2);
+let mut n1 = Bigint::new(vec![rng.gen()]);
+let mut n2 = Bigint::new(vec![rng.gen()]);
+let mut d = Bigint::new(vec![rng.gen()]);
+
+for i in 0..4 {
+    n1 = n1 << 1;
+    n1 += Bigint::new(vec![rng.gen()]); 
+}
+
+for i in 0..2 {
+    n2 = n2 << 1;
+    n2 += Bigint::new(vec![rng.gen()]); 
+}
+
+for i in 0..2 {
+    d = d << 1;
+    d += Bigint::new(vec![rng.gen()]); 
+}
+
+
+println!("{:016x}", n1[0]);
+let a = Bigint::first_digit_with_index(n1[0]);
+println!("{:016x} at index {}", a.0, a.1);
 
 }
