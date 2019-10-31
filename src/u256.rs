ca128b75fe63770320f1a2de2e4808da01260b16
Joshua Fitzgerald <joshuabfitzgerald@gmail.com>
2019-10-30 21:52:17 -0400

    Began modular multiiplication algorithm

--- src/u256.rs
+++ src/u256.rs
@@ -12,8 +12,6 @@ pub struct U256 {
     pub list: [u64; 4],
 }
 
-
-
 impl U256 {
     pub fn new (list: [u64; 4]) -> U256 {
         U256 {
@@ -80,6 +78,18 @@ impl U256 {
         (U256::new(result), U256::new(borrows))
     }
 
+    pub fn recursing_mul_mod (upper: u64, lower: U256, divisor: U256) -> (u64, U256) {
+        let hlr = (U256::max() % divisor) + U256::one();
+        println!("hlr: {}", hlr);
+        let upp_res = hlr * upper;
+        println!("up0: {}", upp_res.0);
+        println!("up1: {}", upp_res.1);        
+        let ra = U256::recursing_add(lower, upp_res.1);
+        println!("ra.0 {}", ra.0);
+        println!("ra.1 {}", ra.1);
+        (ra.1[3] + upp_res.0, ra.0) 
+    }
+
     pub fn mul_u64_with_carry (left: u64, right: u64) -> (u64, u64) {
         let sqrt = 0x100000000;
         let leftq = [left / sqrt, left % sqrt];
@@ -94,80 +104,6 @@ impl U256 {
             (big, littles.0)
         }
     }
-/*
-    pub fn recursing_mul (self, other: U256) -> (U256, U256) {
-        let mut prod = [0; 8];
-            for i in (0..4).rev() {
-                for j in (0..4).rev() {
-                    let mc = U256::mul_u64_with_carry(self[i], other[j]);
-                    let ac1 = overflowing_add(prod[i+j+1], mc.1);
-                    let ac2 = overflowing_add(prod[i+j], mc.0);
-
-                    prod[i+j+1] += ac1.0;
-                    prod[i+j] += ac1.1 as u64;
-                    prod[i+j] += ac2.0;
-                    prod[i+j-1] += ac2.1 as u64;
-                }
-            }
-        (U256::one(), U256::one())
-    }
-*/
-
-    pub fn low_mul (self, other: U256) -> U256 {
-        let mut p = [(0,0); 4];
-        let mut res = [0; 4];
-        let mut car = [0; 4];
-        for i in (2..4).rev() {
-            for j in (2..4).rev() {
-                p[i] = U256::mul_u64_with_carry(self[i] , other[j]);
-                res[i + j - 3] = p[i].1;
-                car[i + j - 4] = p[i].0;
-            }
-        }
-        U256::new(res) + U256::new(car)
-    }
-
-    pub fn left_mid_mul (self, other: U256) -> U256 {
-        let mut p = [(0,0); 4];
-        let mut res = [0; 4];
-        let mut car = [0; 4];
-        for i in (2..4).rev() {
-            for j in (0..2).rev() {
-                p[i] = U256::mul_u64_with_carry(self[i] , other[j]);
-                res[i + j - 1] = p[i].1;
-                car[i + j - 2] = p[i].0;
-            }
-        }
-        U256::new(res) + U256::new(car)
-    }
-
-    pub fn right_mid_mul (self, other: U256) -> U256 {
-        let mut p = [(0,0); 4];
-        let mut res = [0; 4];
-        let mut car = [0; 4];
-        for i in (0..2).rev() {
-            for j in (2..4).rev() {
-                p[i] = U256::mul_u64_with_carry(self[i] , other[j]);
-                res[i + j - 1] = p[i].1;
-                car[i + j - 2] = p[i].0;
-            }
-        }
-        U256::new(res) + U256::new(car)
-    }
-
-    pub fn hi_mul (self, other: U256) -> U256 {
-        let mut p = [(0,0); 4];
-        let mut res = [0; 4];
-        let mut car = [0; 4];
-        for i in (0..2).rev() {
-            for j in (0..2).rev() {
-                p[i] = U256::mul_u64_with_carry(self[i] , other[j]);
-                res[i + j + 1] = p[i].1;
-                car[i + j] = p[i].0;
-            }
-        }
-        U256::new(res) + U256::new(car)
-    }
 
     pub fn len (self) -> usize {
         let mut result: usize = 256;
@@ -179,12 +115,36 @@ impl U256 {
         result -= self[i].leading_zeros() as usize;
         result
     }
+
+    pub fn mul_mod (self, other: Self, divisor: Self) -> U256 {
+        let hlr = (U256::max() % divisor) + 1;
+        let mut res = U256::zero();
+        //for i in (0..4).rev() {
+            //let uprod = self * other[i];
+            //res += uprod.1 % divisor;
+            //let hi_par = uprod.0
+            //res += mul_mod(hlr * uprod.0)
+        //}
+        U256::zero()
+    }
 }
 
 impl ops::Add for U256 {
     type Output = Self;
     fn add (self, other: Self) -> U256 {
         let mut rc = U256::recursing_add(self, other);
+        while rc.1 > U256::zero() {
+            rc = U256::recursing_add(rc.0, rc.1);
+        }
+        rc.0
+    }
+}
+
+
+impl ops::Add<u64> for U256 {
+    type Output = Self;
+    fn add (self, other: u64) -> U256 {
+        let mut rc = U256::recursing_add(self, U256::new([0,0,0,other]));
         while rc.1 > U256::zero() {
             rc = U256::recursing_add(rc.0, rc.1);
         }
@@ -264,10 +224,21 @@ impl ops::Div for U256 {
     }
 }
 
-impl ops::Mul for U256 {
-    type Output = (Self, Self);
-    fn mul (self, other: Self) -> (U256, U256) {
-        (U256::one(), U256::one())
+impl ops::Mul<u64> for U256 {
+    type Output = (u64, Self);
+    fn mul (self, other: u64) -> (u64, U256) {
+        let mut lo_r: [u64; 4] = [0; 4];
+        for i in (1..4).rev() {
+            let mc = U256::mul_u64_with_carry(self[i], other);
+            let ra = lo_r[i].overflowing_add(mc.1);
+            lo_r[i] = ra.0;
+            lo_r[i-1] += ra.1 as u64 + mc.0;
+        }
+        let mc = U256::mul_u64_with_carry(self[0], other);
+        let ra = lo_r[0].overflowing_add(mc.1);
+        lo_r[0] = ra.0;
+        let hi_r = ra.1 as u64 + mc.0;
+        (hi_r, U256::new(lo_r))
     }
 }
 
@@ -285,7 +256,7 @@ impl ops::Shl<usize> for U256 {
             let mut result = self.list;
             if r != 0 {
                 let mut lt: u64 = 0;
-                let mut rt: u64 = 0;
+                let mut rt: u64;
                 for i in (0..4).rev() {
                     rt = self[i] << r;
                     result[i] = rt + lt;
