2bc4676d2aa902a1c31a3c2c72eae127a3cb6edf
Joshua Fitzgerald <joshuabfitzgerald@gmail.com>
2019-11-04 19:49:39 -0500

    Change name of PrimeFieldElement

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod fields;
 
 use fields::U256;
 use fields::PrimeField;
-use fields::FieldElement;
+use fields::PrimeFieldElement;
 
 fn main () {
 
@@ -13,8 +13,8 @@ let n2 = U256::new([0x734ab324171e6862, 0x5a31a671d3d0f58f, 0x931d7743364beec8, 
 let d =  U256::new([0x09a762d2d4e6dba2, 0x3fa7c65d90e7c587, 0xa16e4fd39b5ecd04, 0x476ae7bb64802287]);
 
 let f = PrimeField::new(d);
-let p1 = FieldElement::new(n1, f);
-let p2 = FieldElement::new(n2, f);
+let p1 = PrimeFieldElement::new(n1, f);
+let p2 = PrimeFieldElement::new(n2, f);
 
 println!("p1:     {}", p1);
 println!("p2:     {}", p2);
